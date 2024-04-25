fbde24ba284018788f2c95af704deb638cedb446
YilunChen12 <49909007+YilunChen12@users.noreply.github.com>
2024-04-25 21:58:13 +0200

    Sorting Layer

--- TycoonPrototype/Assets/Code/NPCBehavior/NormalFSMStates/IdleState.cs
+++ TycoonPrototype/Assets/Code/NPCBehavior/NormalFSMStates/IdleState.cs
@@ -6,10 +6,10 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 public class IdleState : BaseState
 {
     private Vector3 initialPosition;
-    private float wanderRadius = 5f; 
-    private float wanderTimer; 
-    private float minWaitTime = 1f; 
-    private float maxWaitTime = 2f; 
+    private float wanderRadius = 5f; // ���������뾶
+    private float wanderTimer; // ����������ʱ��
+    private float minWaitTime = 1f; // ��С�ȴ�ʱ��
+    private float maxWaitTime = 2f; // ���ȴ�ʱ��
 
     private Guest attachedGuest;
     public override void EnterState(object obj)
@@ -17,10 +17,10 @@ public class IdleState : BaseState
         Debug.Log("EnterIdleState");
 
         attachedGuest = (Guest)obj;
-        
+        // ��ȡ��ɫ��ʼλ��
         initialPosition = attachedGuest.transform.position;
 
-        
+        // ��ʼ��������ʱ��
         wanderTimer = Random.Range(minWaitTime, maxWaitTime);
     }
 
@@ -31,30 +31,30 @@ public class IdleState : BaseState
 
     public override void OnUpdate()
     {
-        
+        // ����������ʱ��
         wanderTimer -= Time.deltaTime;
         Wander();
 
-        
+        // ���������ʱ��С�ڵ���0��ִ��������Ϊ
         if (wanderTimer <= 0f)
         {
             
-            wanderTimer = Random.Range(minWaitTime, maxWaitTime); 
+            wanderTimer = Random.Range(minWaitTime, maxWaitTime); // ��������������ʱ��
         }
     }
 
     private void Wander()
     {
-        
+        // �������ƫ����
         Vector3 randomOffset = Random.insideUnitSphere * wanderRadius;
 
-        
+        // �����ƫ����Ӧ�õ���ʼλ�ø���
         Vector3 targetPosition = initialPosition + randomOffset;
 
-        
-        float moveSpeed = 5f; 
+        // �����ƶ��ٶ�
+        float moveSpeed = 5f; // ���Ը�����Ҫ�����ƶ��ٶ�
 
-        
+        // �ƶ���ɫ��Ŀ��λ��
         attachedGuest.transform.position = Vector3.MoveTowards(attachedGuest.transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
 
