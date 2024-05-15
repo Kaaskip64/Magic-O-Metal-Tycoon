a953d9e5fc97a26025b3f3041dbc8f5ae1d5413a
Jacco Verhoeckx <jaccozakelijk@hotmail.com>
2024-05-15 16:46:53 +0200

    Camera Boundaries Added (as well as cinemachine as a whole)

--- TycoonPrototype/Assets/Code/CameraController.cs
+++ TycoonPrototype/Assets/Code/CameraController.cs
@@ -1,30 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    [Header("Panning")]
     public float panSpeed = 20f;
     public float panBorderThickness = 10f;
 
+    [Space(20)]
+
+    [Header("Zoom Boundaries (dont make min bigger than max or suffer the consequences)")]
+    [Range(5, 225)]
     public int minZoom = 5;
+    [Range(5, 225)]
     public int maxZoom = 30;
 
+    [Space(30)]
+    [Header("Misc Zoom Variables")]
     public float zoomSpeed = 10;
     public float zoomSensitivity = 5f;
 
-    private float zoomTarget;
-
+    [Header("Camera State")]
     public bool cameraActive;
+    [Header("Virtual Camera")]
+    public CinemachineVirtualCamera vcam;
+
+    private float zoomTarget;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
-        zoomTarget = Camera.main.orthographicSize;
+        zoomTarget = vcam.m_Lens.OrthographicSize;
+
+        if(minZoom > maxZoom)
+        {
+            Debug.LogError("Zoom min is bigger than max. Now face my 1000 year mind prison attack");
+        }
     }
 
     void Update()
     {
+        if (minZoom > maxZoom)
+        {
+            return;
+        }
+
         if(cameraActive)
         {
             CameraMove();
@@ -34,7 +56,7 @@ public class CameraController : MonoBehaviour
 
     void CameraMove()
     {
-        Vector3 pos = transform.position;
+        Vector3 pos = Camera.main.transform.position;
         if (Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             pos.y += panSpeed * Time.deltaTime;
@@ -51,13 +73,14 @@ public class CameraController : MonoBehaviour
         {
             pos.x -= panSpeed * Time.deltaTime;
         }
-        transform.position = pos;
+        vcam.transform.position = pos;
 
         zoomTarget -= Input.mouseScrollDelta.y * zoomSensitivity;
         zoomTarget = Mathf.Clamp(zoomTarget, minZoom, maxZoom);
-        float newSize = Mathf.MoveTowards(Camera.main.orthographicSize, zoomTarget, zoomSpeed * Time.deltaTime);
-        Camera.main.orthographicSize = newSize;
+        float newSize = Mathf.MoveTowards(vcam.m_Lens.OrthographicSize, zoomTarget, zoomSpeed * Time.deltaTime);
+        vcam.m_Lens.OrthographicSize = newSize;
     }
 
     
 }
+
